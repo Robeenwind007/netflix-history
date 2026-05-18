4c8e80b23ccecbff3cadab1ade1bde4588c49cf4
Olivier BERNARD <obernard@herculepro.com>
2026-05-18 14:57:04 +0200

    fix: GitHub Pages subpath /netflix-history/

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'nethistory-v1';
+const CACHE = 'nethistory-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  '/netflix-history/',
+  '/netflix-history/index.html',
+  '/netflix-history/manifest.json',
+  '/netflix-history/icons/icon-192.png',
+  '/netflix-history/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap'
 ];
 
